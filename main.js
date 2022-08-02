4d893511a9d4eccc8fc18c7b1f0897f7d8b708b8
Orlando <carlos.orlando.rios@gmail.com>
2022-08-02 15:30:26 -0500

    added api info in javascript

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-console.log('connected!')
-
-let searchBar = document.createElement('div')
-searchBar.classList.add('searchBar')
-container.appendChild(searchBar)
-
-let searchButton = document.createElement('div')
-searchButton.innerText = search
+const container = document.querySelector('.container')
 
 
 let resultsDiv = document.querySelector('#results')
@@ -23,26 +16,38 @@ fetch(searchUrl, {
     return response.json()
 })
 // data is whatever the aboe code returns, in this case response.json()
+// function for displaying Track info
 .then(function (data){
     let songs  = data.results
     for(let song of songs){
+
+        // create div and class for info
+        let mediaDiv = document.createElement('div')
+        mediaDiv.classList.add('media');
+        
+        // album art div & class
+        let albumCover = document.createElement('img');
+        albumCover.classList.add('albumCover');
+        albumCover.src = `${song.artworkUrl100}`;
+        
+         // artist name div & classlist
+        let artistName = document.createElement('div');
+        artistName.classList.add('artistName');
+        artistName.innerText = `${song.artistName}`;
+        
+        // track title div & classlist
+        let trackTitle = document.createElement('div');
+        trackTitle.classList.add('trackTitle');
+        trackTitle.innerText = `${song.trackName}`;
+    
+        mediaDiv.appendChild(albumCover)
+        mediaDiv.appendChild(artistName)
+        mediaDiv.appendChild(trackTitle)
+        container.appendChild(mediaDiv)
+
+    
     }
 })
-// function for displaying song results
 
-function showSongName(songArray){
-    for (let song of songArray)
-        let songReturnDiv = document.createElement('div')
-        songReturnDiv.classList.add('songReturn')
-
-        //div for song thumbnail & class
-        let trackCover = document.createElement('div')
-        trackCover.classList.add('trackCover')
-        trackCover.src = `${song.artworkUrl100}`
-
-        // song title with div & class
-        let songTitle = document.createElement('div')
-
-}
 
 
