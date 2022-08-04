f620a609ad4b4e03f246cf6a4ba80f2e5c1725b4
Orlando <carlos.orlando.rios@gmail.com>
2022-08-04 13:06:25 -0500

    accomplished a bunch of css for page

--- main.js
+++ main.js
@@ -1,53 +1,78 @@
 const container = document.querySelector('.container')
 
-
 let resultsDiv = document.querySelector('#results')
 console.log('results div', resultsDiv)
 
-let searchUrl = 'https://itunes.apple.com/search?term=because%2Bthe%2Binternet'
+let searchBaseUrl = 'https://itunes.apple.com/search?term='
 
-fetch(searchUrl, {
-    method: 'GET',
-    Headers: {'Content-Type': 'application/json'}
+let searchBar = document.querySelector('#search-bar')
+
+searchBar.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let searchBox = document.querySelector('#search-box')
+    let searchUrl = `${searchBaseUrl}${searchBox.value}`
+    console.log('search url', searchUrl)
+    getSearchResults(searchUrl)
+
 })
 
-// response is whatever the fetch return
-.then(function (response){
-    return response.json()
-})
-// data is whatever the aboe code returns, in this case response.json()
-// function for displaying Track info
-.then(function (data){
-    let songs  = data.results
-    for(let song of songs){
+// search function
+function getSearchResults(url){
+    fetch(url, { 
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
+    })
+        // response is whatever the fetch return
+        .then(function (response){
+            return response.json()
+        })
+        
+        // data is whatever the code returns, in this case response.json()
+        // function for displaying Track info
+        .then(function (data){
+            let songs  = data.results
+            console.log(songs)
+            for(let song of songs){
+        
+                // create div and class for info
+                let informationDiv = document.createElement('div')
+                informationDiv.classList.add('informationDiv');
+                
+                // album art div & class
+                let albumCover = document.createElement('img');
+                albumCover.classList.add('albumCover');
+                albumCover.src = `${song.artworkUrl100}`;
+                
+                 // artist name div & classlist
+                let artistName = document.createElement('div');
+                artistName.classList.add('artistName');
+                artistName.innerText = `${song.artistName}`;
+                
+                // track title div & classlist
+                let trackTitle = document.createElement('div');
+                trackTitle.classList.add('trackTitle');
+                trackTitle.innerText = `${song.trackName}`;
+        
+                // track album name div & classlist
+                let albumName = document.createElement('div');
+                albumName.classList.add('albumName')
+                albumName.innerText = `${song.collectionName}`;
 
-        // create div and class for info
-        let mediaDiv = document.createElement('div')
-        mediaDiv.classList.add('media');
-        
-        // album art div & class
-        let albumCover = document.createElement('img');
-        albumCover.classList.add('albumCover');
-        albumCover.src = `${song.artworkUrl100}`;
-        
-         // artist name div & classlist
-        let artistName = document.createElement('div');
-        artistName.classList.add('artistName');
-        artistName.innerText = `${song.artistName}`;
-        
-        // track title div & classlist
-        let trackTitle = document.createElement('div');
-        trackTitle.classList.add('trackTitle');
-        trackTitle.innerText = `${song.trackName}`;
-    
-        mediaDiv.appendChild(albumCover)
-        mediaDiv.appendChild(artistName)
-        mediaDiv.appendChild(trackTitle)
-        container.appendChild(mediaDiv)
-
-    
+                // Audio & src & controls
+                let audioPreview = document.createElement('audio');
+                audioPreview.classList.add('audioPreview');
+                audioPreview.src = song.previewUrl;
+                audioPreview.controls = true;
+                
+            
+                informationDiv.appendChild(albumCover)
+                informationDiv.appendChild(artistName)
+                informationDiv.appendChild(trackTitle)
+                informationDiv.appendChild(albumName)
+                informationDiv.appendChild(audioPreview)
+                container.appendChild(informationDiv)
+            
+            }
+        })
     }
-})
-
-
-
+   
